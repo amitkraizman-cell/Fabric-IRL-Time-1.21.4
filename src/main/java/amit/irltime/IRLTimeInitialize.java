3b338b2cc2b4f72b79e7cb89ecf6953a7a1e999a
Amit Kraizman <amory1702@gmail.com>
2025-12-02 14:11:39 +0200

    fixed - position of the time on screen

--- src/main/java/amit/irltime/IRLTimeInitialize.java
+++ src/main/java/amit/irltime/IRLTimeInitialize.java
@@ -10,18 +10,25 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class IRLTimeInitialize {
+    static MinecraftClient client;
+    static TextRenderer renderer;
 
     private static final DateTimeFormatter TIME_FORMATTER =
             DateTimeFormatter.ofPattern("HH:mm:ss");
 
     public static void registerIRLTimeInitialize(){
+        client = MinecraftClient.getInstance();
+        renderer = client.textRenderer;
         IRLTime.LOGGER.info("Registering for " + IRLTime.MOD_ID);
         HudRenderCallback.EVENT.register((DrawContext drawContext, RenderTickCounter tickCounter) -> {
             TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
 
             String text = LocalTime.now().format(TIME_FORMATTER);
-            int x = 380;
-            int y = 10;
+            int screenWidth = client.getWindow().getScaledWidth();
+            int textWidth = renderer.getWidth(text);
+            int margin = 10;
+            int x = screenWidth - textWidth - margin;
+            int y = margin;
 
             // color = 0xRRGGBB, last boolean = shadow
             drawContext.drawText(renderer, text, x, y, 0xFFFFFF, true);
