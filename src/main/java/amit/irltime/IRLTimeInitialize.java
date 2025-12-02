843628acd73ecb46e0e1dd096855625b338cef82
Amit Kraizman <amory1702@gmail.com>
2025-12-02 18:32:19 +0200

    added - "Local Time: " string before the time added - 2 functions for future optional 12-hour format time display

--- src/main/java/amit/irltime/IRLTimeInitialize.java
+++ src/main/java/amit/irltime/IRLTimeInitialize.java
@@ -28,7 +28,8 @@ public class IRLTimeInitialize {
         IRLTime.LOGGER.info("Registering for " + IRLTime.MOD_ID);
         HudRenderCallback.EVENT.register((DrawContext drawContext, RenderTickCounter tickCounter) -> {
             TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
-            String text = LocalTime.now().format(TIME_FORMATTER);
+            String localTime= LocalTime.now().format(TIME_FORMATTER);
+            String text ="Local Time: " + localTime;
             int screenWidth = client.getWindow().getScaledWidth();
             int textWidth = renderer.getWidth(text);
             int margin = 10;
@@ -41,6 +42,28 @@ public class IRLTimeInitialize {
             moveText(effects);
             drawContext.drawText(renderer, text, x, y, 0xFFFFFF, true);
         });
+    }
+    //returns "AP" or "PM" accordingly
+    private static String AMorPMString(String localTime){
+        int hour = LocalTime.now().getHour();
+        if (hour>0 && hour<12){
+            System.out.println(hour);
+            System.out.println(localTime);
+            return " AM";
+        }
+        else {
+            System.out.println(hour);
+            System.out.println(localTime);
+            return " PM";
+        }
+    }
+    //returns an integer in a 12-hour format
+    private static int AMorPMInt(String localTime){
+        int hour = LocalTime.now().getHour();
+        if (hour>0 && hour<12){
+            return hour;
+        }
+        else return hour-12;
     }
     //if there's an effect on the player, time moves down
     private static void moveText(Collection<StatusEffectInstance> effects) {
