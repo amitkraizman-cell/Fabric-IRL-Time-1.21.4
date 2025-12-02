a345799d94670f1840c9845bdc21831c051b75e0
Amit Kraizman <amory1702@gmail.com>
2025-12-02 13:17:27 +0200

    Initial commit

---
+++ src/main/java/amit/irltime/IRLTime.java
@@ -0,0 +1,16 @@
+package amit.irltime;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IRLTime implements ModInitializer {
+	public static final String MOD_ID = "irltime";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/amit/irltime/IRLTimeClient.java
@@ -0,0 +1,10 @@
+package amit.irltime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IRLTimeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
