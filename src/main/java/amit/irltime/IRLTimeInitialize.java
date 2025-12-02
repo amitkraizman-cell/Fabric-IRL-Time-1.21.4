7594c9697c3ece501d54890009b33a772b35ea37
Amit Kraizman <amory1702@gmail.com>
2025-12-02 17:48:44 +0200

    fixed - potion or beacon effects overlaying the time

--- src/main/java/amit/irltime/IRLTimeInitialize.java
+++ src/main/java/amit/irltime/IRLTimeInitialize.java
@@ -5,16 +5,22 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collection;
 
 public class IRLTimeInitialize {
     static MinecraftClient client;
     static TextRenderer renderer;
+    static int x;
+    static int y;
 
     private static final DateTimeFormatter TIME_FORMATTER =
             DateTimeFormatter.ofPattern("HH:mm:ss");
+
 
     public static void registerIRLTimeInitialize(){
         client = MinecraftClient.getInstance();
@@ -22,17 +28,40 @@ public class IRLTimeInitialize {
         IRLTime.LOGGER.info("Registering for " + IRLTime.MOD_ID);
         HudRenderCallback.EVENT.register((DrawContext drawContext, RenderTickCounter tickCounter) -> {
             TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
-
             String text = LocalTime.now().format(TIME_FORMATTER);
             int screenWidth = client.getWindow().getScaledWidth();
             int textWidth = renderer.getWidth(text);
             int margin = 10;
-            int x = screenWidth - textWidth - margin;
-            int y = margin;
+            x = screenWidth - textWidth - margin;
+            y = margin;
 
-            // color = 0xRRGGBB, last boolean = shadow
+            assert client.player != null;
+            Collection<StatusEffectInstance> effects = client.player.getStatusEffects();
+
+            moveText(effects);
             drawContext.drawText(renderer, text, x, y, 0xFFFFFF, true);
         });
+    }
+    //if there's an effect on the player, time moves down
+    private static void moveText(Collection<StatusEffectInstance> effects) {
+        boolean hasTopRightEffects = effects.stream().anyMatch(effect ->
+                effect.shouldShowIcon() && !isBottomHudEffect(effect)
+        );
+
+        if (hasTopRightEffects) {
+            int offset = 20;
+            x -= offset;
+        }
+    }
+    //some effects don't appear in the top right
+    private static boolean isBottomHudEffect(StatusEffectInstance effect) {
+        var type = effect.getEffectType();
+        return type == StatusEffects.BAD_OMEN
+                || type == StatusEffects.TRIAL_OMEN
+                || type == StatusEffects.RAID_OMEN
+                || type == StatusEffects.WIND_CHARGED
+                || type == StatusEffects.WEAVING
+                || type == StatusEffects.OOZING;
     }
 
 
